5a42420a27a70cf66d7cff224e5694abf7f31cf4
Ovadiah <230772997+randomcat962codes@users.noreply.github.com>
2025-11-08 20:45:26 -0600

    Create Token class

--- src/lexer/lexer.cs
+++ src/lexer/lexer.cs
@@ -1,5 +1,48 @@
 namespace BleepBloop;
 
+public class Token
+{
+    public enum TokenType
+    {
+        OpenTypeIdentifier,
+        CloseTypeIdentifier,
+
+        TextType,
+        ListType,
+        DictionaryType,
+
+        Quote,
+        Colon,
+        Seperator,
+
+        OpenBracket,
+        CloseBracket,
+
+        OpenSquareBracket,
+        CloseSquareBracket,
+
+        Identifier,
+    }
+
+    private TokenType type;
+    public TokenType Type()
+    {
+        return type;
+    }
+
+    private string value;
+    public string Value()
+    {
+        return value;
+    }
+
+    public Token(TokenType t, string v)
+    {
+        type = t;
+        value = v;
+    }
+}
+
 public static class Lexer
 {
     
